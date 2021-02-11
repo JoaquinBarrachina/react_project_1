a023b35c075655e5eb92f1bcabbd65075fb8cda0
JoaquinBarrachina <mechaximo@gmail.com>
2021-02-11 17:50:23 +0100

    first push

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Loading from "./components/Loding";
+import Tours from "./components/Tours";
+const url = "https://course-api.com/react-tours-project";
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+  const removeTour = (id) => {
+    setTours(tours.filter((tour) => tour.id !== id));
+  };
+  const fetchTours = async () => {
+    setLoading(true);
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+
+      setTours(data);
+      setLoading(false);
+    } catch (e) {
+      setLoading(false);
+      console.log("error");
+    }
+  };
+  useEffect(() => {
+    fetchTours();
+  }, []);
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className="title">
+          <h2>No tours left</h2>
+          <button className="btn" onClick={fetchTours}>
+            Load again
+          </button>
+        </div>
+      </main>
+    );
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <main>
+        <Tours tours={tours} removeTour={removeTour} />
+      </main>
+    </React.Fragment>
   );
 }
 
---
+++ src/components/Loding.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Loding = () => {
+  return (
+    <div className="loading">
+      <h1>Loading...</h1>
+    </div>
+  );
+};
+
+export default Loding;
